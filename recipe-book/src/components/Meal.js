5c2fca57e962cf62401b4fff8f3f6acaef7f028e
Magda Sidor <magda_si@wp.pl>
2018-11-11 00:42:31 +0100

    Add new props to Meal component

--- recipe-book/src/components/Meal.js
+++ recipe-book/src/components/Meal.js
@@ -4,6 +4,8 @@ const Meal = (props) => {
     return(
         <div>
             <h2>{props.strMeal}</h2>
+            <h3>{props.strCategory}</h3>
+            <h3>{props.strArea}</h3>
             <img src={props.strMealThumb} width="300" alt="meal_picture"/>
             <p>{props.strInstructions}</p>
         </div>
